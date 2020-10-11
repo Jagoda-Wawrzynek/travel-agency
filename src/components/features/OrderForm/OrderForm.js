118267d821afbd15bf5af8ae9ac9afc138a6218c
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-10-11 22:07:00 +0200

    Sending orders to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,9 +5,45 @@ import {Grid, Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from './../OrderOption/OrderOption';
 import pircing from './../../../data/pricing.json';
+import Button from './../../common/Button/Button';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+import { formatPrice } from '../../../utils/formatPrice';
+
+const sendOrder = (options, tripCost, tripDetails) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    ...tripDetails,
+  };
+
+  if (options.name != '' && options.contact != '') {
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  } else {
+    alert('You should fill name and contact');
+  }
+};
 
 
-const OrderForm = ({options, tripCost, setOrderOption}) => {
+const OrderForm = ({options, tripCost, setOrderOption, tripDetails}) => {
   return(
     <Grid>
       <Row>
@@ -19,6 +55,7 @@ const OrderForm = ({options, tripCost, setOrderOption}) => {
         <Col xs={12}>
           <OrderSummary options={options} cost={tripCost} />
         </Col>
+        <Button onClick={() => sendOrder(options, tripCost, tripDetails)}>Order now!</Button>
       </Row>
     </Grid>
   );
@@ -28,6 +65,7 @@ OrderForm.propTypes = {
   options: PropTypes.object,
   tripCost: PropTypes.node,
   setOrderOption: PropTypes.func,
+  tripDetails: PropTypes.object,
 };
 
 export default OrderForm;
